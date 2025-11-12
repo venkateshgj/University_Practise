b8ff23eb9842b6ef097fe8cf2960396ab12712f3
VenkateshGJ <venkteshgj@gmail.com>
2025-11-12 09:38:30 +0530

    created Service package

---
+++ src/main/java/com/springboot/University/Service/ProfessorService.java
@@ -0,0 +1,4 @@
+package com.springboot.University.Service;
+
+public interface ProfessorService {
+}
---
+++ src/main/java/com/springboot/University/Service/iCourseService.java
@@ -0,0 +1,7 @@
+package com.springboot.University.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class iCourseService implements CourseService{
+}
