f5ab2435229ab177eb22591318ddb7b4a286745b
VenkateshGJ <venkteshgj@gmail.com>
2025-11-24 01:10:21 +0530

    test case modified

--- src/test/java/com/springboot/University/Controller/CourseControllerTest.java
+++ src/test/java/com/springboot/University/Controller/CourseControllerTest.java
@@ -3,7 +3,10 @@ package com.springboot.University.Controller;
 import com.springboot.University.DTO.CourseDTO;
 import com.springboot.University.Service.CourseService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
+//import org.junit.jupiter.api.extension.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +17,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CourseControllerTest {
 
-    @InjectMocks
-    private CourseController courseController;
+//    @InjectMocks
+//    private CourseController courseController;
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,7 +36,7 @@ public class CourseControllerTest {
     private CourseService courseServiceMock;
 
     @Test
-    public void getCourseBasedOnIdTest(){
+    public void getCourseBasedOnIdTest() throws Exception {
         CourseDTO courseDtoMock = new CourseDTO();
         courseDtoMock.setId(1L);
         courseDtoMock.setTitle("DSA");
@@ -41,10 +47,8 @@ public class CourseControllerTest {
 
         when(courseServiceMock.getCourseById(1L)).thenReturn(courseDtoMock);
 
-        // web mock mvc
-
-        mockMvc.perform(get("/university/api/v1/course/1"))
-                .contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(get("/university/api/v1/course/1")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L));
 
